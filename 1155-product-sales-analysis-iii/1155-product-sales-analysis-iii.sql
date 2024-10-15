dcf25f248ebc00218ca80f5335bb466bc656dc5a
Harsh Rawat <146295753+harshr1711@users.noreply.github.com>
2024-10-15 11:57:51 +0530

    Time: 1052 ms (50.21%), Space: 0B (100%) - LeetHub

---
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with T1 as
+(Select product_id, min(year) as first_year from sales
+group by product_id
+)
+select s.product_id, s.year as first_year, s.quantity , price from Sales as s
+join T1 
+on s.product_id = T1.product_id
+where s.year = T1.first_year
+group by s.product_id , s.quantity , price
